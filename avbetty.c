93ac53aae0954e59567d1539d3b40f187e2ffeb8
AfyaNarh <nhyiraafya1@gmail.com>
2023-06-19 11:38:04 -0700

    betty checks

---
+++ avbetty.c
@@ -0,0 +1,31 @@
+#include “shell.h”
+
+/**
+ * sum_numb -um two numbers.
+ * @a: First number.
+ * @v:econd number.
+ * Return: The sum of a and v.
+ */
+int sum_numb(int a, int v)
+{
+	return (a + v);
+}
+
+/**
+ * main - Entry point.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num1 = 9;
+	int num2 = 11;
+	int result;
+
+	result = sum_numb(num1, num2);
+	printf("The sum is: %d\n", result);
+
+	return (0);
+}
+
+
+
